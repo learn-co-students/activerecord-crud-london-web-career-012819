432ef2d09c4f89beea887c9088bc61f0a0809473
Tominiyi Ajayi <tominiyimunito@Tominiyis-MacBook-Air.local>
2019-02-11 14:01:10 +0000

    Done

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -25,10 +25,10 @@ def can_be_created_with_a_hash_of_attributes
 end
 
 def can_be_created_in_a_block(args= {title: "Home Alone", release_date: 1990})
-  Movie.create do |m|
-    m.title = args[:title]
-    m.release_date = args[:release_date]
-    m.save
+  Movie.create do |x|
+    x.title = args[:title]
+    x.release_date = args[:release_date]
+    x.save
   end
 end
 
